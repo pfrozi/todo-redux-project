1b18f4501625de1284a526034ac3c996a26c7f8e
Pedro Frozi <pedro.frozi@matriz.sicred.com>
2019-02-06 02:41:27 -0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+import { addTodo, toggleTodo, removeTodo } from "./actions";
+
+const Item = ({ remove, toogleDone, todo }) => {
+  const style = { textDecoration: todo.done ? "line-through" : "" };
+  return (
+    <li>
+      <span style={style}>{todo.text}</span>{" "}
+      <button onClick={toogleDone}>{todo.done ? "Undone" : "Done!"}</button>{" "}
+      <button onClick={remove}>Remove</button>
+    </li>
+  );
+};
 
 class App extends Component {
+  state = { newTodo: "" };
+
+  handlerNewTodoChange = ({ target: { value } }) => {
+    this.setState({ newTodo: value });
+  };
+
+  handlerAddOnClick = () => {
+    const { addTodo } = this.props;
+    addTodo(this.state.newTodo);
+    this.state.newTodo = "";
+  };
+
+  handlerDoneOnClick = index => () => {
+    const { toggleTodo } = this.props;
+    toggleTodo(index);
+  };
+
+  handlerRemoveOnClick = index => () => {
+    const { removeTodo } = this.props;
+    removeTodo(index);
+  };
+
   render() {
+    const { newTodo } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>TODO LIST REDUX</h1>
+        <input
+          type="input"
+          name="Todo"
+          value={newTodo}
+          onChange={this.handlerNewTodoChange}
+        />
+        <button onClick={this.handlerAddOnClick}>Add</button>
+        <ul>
+          {this.props.todos.map((todo, i) => (
+            <Item
+              key={i.toString()}
+              toogleDone={this.handlerDoneOnClick(i)}
+              todo={todo}
+              remove={this.handlerRemoveOnClick(i)}
+            />
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+export { App };
+
+const mapStateToProps = state => ({ todos: state.todos });
+
+const mapDispatchToProps = dispatch => ({
+  addTodo: text => dispatch(addTodo(text)),
+  toggleTodo: index => dispatch(toggleTodo(index)),
+  removeTodo: index => dispatch(removeTodo(index))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
